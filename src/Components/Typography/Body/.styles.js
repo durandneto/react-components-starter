e0b4e712a4a15b8667d820d88183c628a7c0372a
Jose Alves Durand Neto <durand.neto@gmail.com>
2020-04-06 12:39:09 -0700

    adding colors

--- src/Components/Typography/Body/.styles.js
+++ src/Components/Typography/Body/.styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 import Color from '../../Colors'
 
-export const Body = styled.p`
+export const Body = styled.span`
   margin: 0;
   padding: 0;
   font-weight: 400;
--- src/Components/Typography/Body/stories.js
+++ src/Components/Typography/Body/stories.js
@@ -6,7 +6,7 @@ import Body from '.';
 storiesOf('Typography|Body/Default', module)
   .add('Regular', () => (
     <div style={{ padding: "24px" }}>
-      <Body xs color="Indigo-A400" label="XS - There is something about parenthood that gives us a sense of history and a deeply rooted desire to send on into the next generation the great things we have discovered about life.
+      <Body xs label="XS - There is something about parenthood that gives us a sense of history and a deeply rooted desire to send on into the next generation the great things we have discovered about life.
       Your fascination with the universe and how to explore it as we so often do in the field of astronomy can be highly academic and dry as maybe it was if you took a course in astronomy." />
       <br />
       <br />
@@ -39,5 +39,46 @@ storiesOf('Typography|Body/Default', module)
       <br />
       <Body reverse lg label="body - There is something about parenthood that gives us a sense of history and a deeply rooted desire to send on into the next generation the great things we have discovered about life.
       Your fascination with the universe and how to explore it as we so often do in the field of astronomy can be highly academic and dry as maybe it was if you took a course in astronomy." />
+    </div>
+  ))
+
+  .add('Colorful', () => (
+    <div style={{ padding: "24px" }}>
+      <Body color={"Indigo-600"} xs label="body - There is something about parenthood that gives us a sense of history and a deeply rooted desire to send on into the next generation the great things we have discovered about life.
+      Your fascination with the universe and how to explore it as we so often do in the field of astronomy can be highly academic and dry as maybe it was if you took a course in astronomy." />
+    <br />
+      <br />
+      <Body color={"Indigo-700"} sm label="body - There is something about parenthood that gives us a sense of history and a deeply rooted desire to send on into the next generation the great things we have discovered about life.
+      Your fascination with the universe and how to explore it as we so often do in the field of astronomy can be highly academic and dry as maybe it was if you took a course in astronomy." />
+    <br />
+      <br />
+      <Body color={"Indigo-800"} md label="body - There is something about parenthood that gives us a sense of history and a deeply rooted desire to send on into the next generation the great things we have discovered about life.
+      Your fascination with the universe and how to explore it as we so often do in the field of astronomy can be highly academic and dry as maybe it was if you took a course in astronomy." />
+    <br />
+      <br />
+      <Body color={"Indigo-900"} lg label="body - There is something about parenthood that gives us a sense of history and a deeply rooted desire to send on into the next generation the great things we have discovered about life.
+      Your fascination with the universe and how to explore it as we so often do in the field of astronomy can be highly academic and dry as maybe it was if you took a course in astronomy." />
+      <br />
+      <br />
+      <Body color={"Light-Green-600"} xs label="body - There is something about parenthood that gives us a sense of history and a deeply rooted desire to send on into the next generation the great things we have discovered about life.
+      Your fascination with the universe and how to explore it as we so often do in the field of astronomy can be highly academic and dry as maybe it was if you took a course in astronomy." />
+    <br />
+      <br />
+      <Body color={"Light-Green-700"} sm label="body - There is something about parenthood that gives us a sense of history and a deeply rooted desire to send on into the next generation the great things we have discovered about life.
+      Your fascination with the universe and how to explore it as we so often do in the field of astronomy can be highly academic and dry as maybe it was if you took a course in astronomy." />
+    <br />
+      <br />
+      <Body color={"Light-Green-800"} md label="body - There is something about parenthood that gives us a sense of history and a deeply rooted desire to send on into the next generation the great things we have discovered about life.
+      Your fascination with the universe and how to explore it as we so often do in the field of astronomy can be highly academic and dry as maybe it was if you took a course in astronomy." />
+    <br />
+      <br />
+      <Body color={"Light-Green-900"} lg label="body - There is something about parenthood that gives us a sense of history and a deeply rooted desire to send on into the next generation the great things we have discovered about life.
+      Your fascination with the universe and how to explore it as we so often do in the field of astronomy can be highly academic and dry as maybe it was if you took a course in astronomy." />
+      <br />
+      <br />
+      <Body lg label="Deep-Orange-500" color={'Deep-Orange-500'} />
+      <br />
+      <br />
+      <Body lg label="Red-700" color={'Red-700'} />
     </div>
   ))
